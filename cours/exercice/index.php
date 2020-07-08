6af28950578c6bad8256388b5f4fe95c5b2ee373
GueddaSabrine <66629944+GueddaSabrine@users.noreply.github.com>
2020-07-09 00:11:20 +0200

    commit to exo

--- cours/exercice/index.php
+++ cours/exercice/index.php
@@ -1,5 +1,5 @@
 <?php
-?>
+//?>
 
 <!doctype html>
 <html lang="en">
@@ -9,62 +9,40 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Conversion</title>
-    <style>
-        .red {
-            color: red;
-        }
 
-        .vert {
-            color: green;
-        }
-    </style>
 </head>
 <body>
 <div>
-    <form method="post">
-        <p>
-            <label for="">Temperature en °C :</label>
-            <input type="text" id="celcius" name="celsius"/>
-            <input type="submit" value="Convertir"/>
-
-            <?php
-            $celsius = $_POST['celsius'];
-            $farhen = 0;
-
-            if (!empty($celsius) and is_float($celsius)) {
-//                $farhen = floatval($celsius) * 9 / 5 + 32;
-                $farhen = (float)($celsius) * 9 / 5 + 32;
-                echo "<p class='vert'>" . $celsius . " °C = " . $farhen . " °F </p>";
-            } else {
-                exit("<p class='red'>Programme arreté suit à un soucis</p>");
-            }
-            ?>
-        </p>
-    </form>
-</div>
-
-<h3>Calculatrice</h3>
-<form  method="POST">
-    <!-- opérande 1 -->
-
-    <input placeholder="un nombre" type="number" step="any" name="op1" />
-
-    <!-- opération -->
-
-    <select name="operation">
-        <option value="+">+</option>
-        <option value="-">-</option>
-        <option value="x">x</option>
-        <option value="/">/</option>
+<form method="post">
+    Entrez nombre A <input name="A"/>
+    <select name="operator">
+        <option>+</option>
+        <option>-</option>
+        <option>*</option>
+        <option>/</option>
     </select>
+    Entrez nombre A <input name="A"/>
+    <input name="submit" name="cal" valeur="="/>
 
-    <!-- opérande 2 -->
+   <?php
 
-    <input placeholder="un nombre" type="number" step="any" name="op2" />
+   if (isset($_POST['cal'])) {
+       $A = $_POST['A'];
+       $B = $_POST['B'];
+       if ($_POST['operator'] == '+')
+           echo $A + $B;
+       else if ($_POST['operator'] == '-')
+           echo $A - $B;
+       else if ($_POST['operator'] == '*')
+           echo $A * $B;
+       else if ($_POST['operator'] == '/')
+           echo $A / $B;
+   }
 
-    <!-- bouton -->
-
-    <button type="submit" name="soumis"> Calculer</button>
-
+   ?>
+</form>
+</div>
 </body>
 </html>
+
+
